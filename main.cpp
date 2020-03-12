b01f833f73c7c2a0bc6114731be46e7745d979c4
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-03-12 20:20:11 +0800

    update

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+using namespace std;
+
+#define INT(n) int n;scanf("%d", &n);
+#define FOR(i, n) for(int i = 0; i < n; i ++)
+
+int main() {
+    INT(t)
+    FOR(i, t) {
+        INT(n)
+        int sum = 0;
+        int zero_count = 0;
+        FOR(i, n) {
+            INT(a);
+            if (a == 0) {
+                zero_count ++;
+            }
+            sum += a;
+        }
+        int result = zero_count;
+        sum += zero_count;
+        if (sum == 0) {
+            result ++;
+        }
+        printf("%d\n", result);
+    }
+    return 0;
+}
