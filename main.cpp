9d246cd1e0a5b366e04ea73284b7425e61cefe8c
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-03-13 13:43:20 +0800

    fixup

--- main.cpp
+++ main.cpp
@@ -7,31 +7,30 @@
 #include <cstring>
 using namespace std;
 
+#define LL long long
 #define INT(n) int n;scanf("%d", &n);
 #define INTs(xs, n) for(int i = 0; i < n; i++) scanf("%d", &xs[i]);
 #define FOR(i, n) for(int i = 0; i < n; i ++)
 
 int main() {
-    INT(t)
-    char as[101], bs[101], cs[101];
-    FOR(cas, t) {
-        scanf("%s", as);
-        scanf("%s", bs);
-        scanf("%s", cs);
-        int n = strlen(as);
-        bool ok = true;
-        FOR(i, n) {
-            if (cs[i] == as[i] || cs[i] == bs[i]) {
-                continue;
-            }
-            ok = false;
-            break;
-        }
-        if (ok) {
-            printf("YES\n");
+    INT(n)
+    int as[200005], bs[200008], xs[200009];
+    INTs(as, n)
+    INTs(bs, n)
+    FOR(i, n) {
+        xs[i] = as[i] - bs[i];
+    }
+    sort(xs, xs+n);
+    LL sum = 0;
+    int j = n;
+    FOR(i, n) {
+        while (j > 0 && xs[i] + xs[j-1] > 0) j--;
+        if (j <= i) {
+            sum += (n - i - 1);
         } else {
-            printf("NO\n");
+            sum += (n - j);
         }
     }
+    printf("%lld\n", sum);
     return 0;
 }
