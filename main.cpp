2c43f8b7a3a1a3e7a89df8355c9f279b414bf80d
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-03-13 23:20:06 +0800

    fixup

--- main.cpp
+++ main.cpp
@@ -4,39 +4,29 @@
 #include <stdio.h>
 #include <algorithm>
 #include <stdlib.h>
+#include <map>
 #include <cstring>
 using namespace std;
 
 #define LL long long
 #define INT(n) int n;scanf("%d", &n);
 #define INTs(xs, n) for(int i = 0; i < n; i++) scanf("%d", &xs[i]);
-#define F0R(i, n) for(int i = 0; i < n; i ++)
-#define FOR(i, b, e) for(int i = b; i < e; i++)
+#define FOR(i, n) for(int i = 0; i < n; i ++)
+#define FOR1(i, b, e) for(int i = b; i < e; i++)
+#define FORMAP(m, it) for(auto it = m.begin(); it != m.end(); it++)
 #define COPY(dst, src, n) memcpy(dst, src, sizeof((dst)[0])*n);
 #define RESET(xs, n, v) for(int i = 0; i < n; i++) xs[i] = v;
 
 int main() {
-    INT(t)
-    char s[5005], tmp[5005], best[5005];
-    F0R(cas, t) {
-        INT(n)
-        scanf("%s", s);
-        int bestk = -1;
-        FOR(k, 1, n+1) {
-            COPY(tmp, s+k-1, n-k+1)
-            COPY(tmp+n-k+1, s, k-1)
-            tmp[n] = '\0';
-            if ((n-k+1)&1) {
-                reverse(tmp+n-k+1, tmp+n);
-            }
-            if (bestk == -1 || (strcmp(tmp, best) < 0)) {
-                bestk = k;
-                COPY(best, tmp, n)
-                best[n] = '\0';
-            }
-        }
-        printf("%s\n", best);
-        printf("%d\n", bestk);
-    }
+    INT(n)
+    int bs[200006], xs[200006];
+    INTs(bs, n)
+    FOR(i, n) xs[i] = bs[i]-i;
+    map<int, LL> m;
+    m[xs[0]] = bs[0];
+    FOR1(i, 1, n) m[xs[i]] += bs[i];
+    LL ret = 0;
+    FORMAP(m, it) ret = max(ret, it->second);
+    printf("%lld\n", ret);
     return 0;
 }
