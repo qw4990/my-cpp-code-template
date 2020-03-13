7876562c4edc952cdd86d5ac9adfd2a4dc7a496d
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-03-13 11:30:11 +0800

    fixup

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <algorithm>
 #include <stdlib.h>
+#include <cstring>
 using namespace std;
 
 #define INT(n) int n;scanf("%d", &n);
@@ -12,12 +13,25 @@ using namespace std;
 
 int main() {
     INT(t)
-    int as[200005];
+    char as[101], bs[101], cs[101];
     FOR(cas, t) {
-        INT(n)
-        INTs(as, 2*n)
-        sort(as, as+2*n);
-        printf("%d\n", abs(as[n-1]-as[n]));
+        scanf("%s", as);
+        scanf("%s", bs);
+        scanf("%s", cs);
+        int n = strlen(as);
+        bool ok = true;
+        FOR(i, n) {
+            if (cs[i] == as[i] || cs[i] == bs[i]) {
+                continue;
+            }
+            ok = false;
+            break;
+        }
+        if (ok) {
+            printf("YES\n");
+        } else {
+            printf("NO\n");
+        }
     }
     return 0;
 }
