8aaff0d7e7506f72a48cce861a0f8f6b638d2b3c
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-03-13 11:03:25 +0800

    fixup

--- main.cpp
+++ main.cpp
@@ -2,30 +2,23 @@
 #include <map>
 #include <vector>
 #include <stdio.h>
+#include <algorithm>
 using namespace std;
 
 #define INT(n) int n;scanf("%d", &n);
+#define INTs(xs, n) for(int i = 0; i < n; i++) scanf("%d", &xs[i]);
 #define FOR(i, n) for(int i = 0; i < n; i ++)
 
 int main() {
     INT(t)
-    FOR(i, t) {
+    int as[100005];
+    FOR(cas, t) {
         INT(n)
-        int sum = 0;
-        int zero_count = 0;
-        FOR(i, n) {
-            INT(a);
-            if (a == 0) {
-                zero_count ++;
-            }
-            sum += a;
-        }
-        int result = zero_count;
-        sum += zero_count;
-        if (sum == 0) {
-            result ++;
-        }
-        printf("%d\n", result);
+        INTs(as, n)
+        sort(as, as+n);
+        if ((n & 1) > 0) {
+            
+        } 
     }
     return 0;
 }
