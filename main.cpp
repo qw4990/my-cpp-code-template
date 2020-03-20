ae978996ce20f6fb9d957bd743d44c2e41c9f6fd
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-03-20 23:21:44 +0800

    fixup

--- main.cpp
+++ main.cpp
@@ -225,9 +225,41 @@ LL GCD(LL a, LL b) {
 // ############################ strings ############################
 // ##################################################################
 
-// KMP
+// Calculate the next vector used by KMP Algorithm.
+// str:  a b a b a b z a b a b a b a
+// next: 0 0 1 2 3 4 0 1 2 3 4 5 6 ?
+void kmpNext(VEC<char> &str, VEC<int> &next) {
+    int n = str.size();
+    next.resize(n-1);
+    next[0] = 0;
+    int max_len = 0;
+    FOR(i, 1, n-1) {
+        while (max_len > 0 && str[i] != str[max_len])
+            max_len = next[max_len - 1];
+        if (str[max_len] == str[i]) max_len++;
+        next[i] = max_len;
+    }
+}
 
-// KMP Next
+// The KMP Algorithm.
+// str:         a b a b a b z a b a b a b
+// substr:      a b a
+// position:    0   2         7   9
+void kmp(VEC<char> &str, VEC<char> &substr, VEC<int> &position) {
+    VEC<int> next;
+    kmpNext(substr, next);
+    position.clear();
+    int n = str.size(), max_len = 0;
+    REP(i, n) {
+        while (max_len > 0 && str[i] != substr[max_len])
+            max_len = next[max_len - 1];
+        if (str[i] == substr[max_len]) max_len ++;
+        if (max_len == substr.size()) {
+            position.push_back(i - substr.size() + 1);
+            max_len = next[max_len - 1];
+        }
+    }
+}
 
 // Manacher algorithm is used to calculate palindrome strings.
 // All int elements in the original str vectors should be larger than 0.
