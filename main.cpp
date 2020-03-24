086c21503a269c3072d230f94493807d5056146d
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-03-24 10:35:10 +0800

    fixup

--- main.cpp
+++ main.cpp
@@ -24,20 +24,26 @@ using namespace std;
 #define COPY(dst, src, n) for(int i = 0; i < n; i++) {*dst=*src;dst++;src++;}
 #define RESET(begin, end, v) for(auto it = begin; it != end; it++) *it=v;
 #define VEC vector
-#define MATRIX(T, ma, n, m) vector<vector<T>> ma(n, vector<T>(m));
 #define INPUT_VEC(vs, n) vs.resize(n); for(int i = 0; i < n; i++) cin >> vs[i];
 #define OUTPUT_VEC(vs) for(auto it=vs.begin(); it != vs.end(); it++) cout << *it << " "; cout << endl;
 #define OUTPUT_BE(begin, end) for(auto it = begin; it != end; it++) cout << *it << " "; cout << endl;
-#define PII pair<int, int>
-#define PLL pair<long long, long long>
 #define FUNCTOR(name, ret, args, body) struct name {ret operator() args body};
-#define PQ priority_queue
 #define CLOCK_BEGIN() clock_t _clock_begin_ = clock();
 #define CLOCK_END() clock_t _clock_end_ = clock(); cout << "clock" << ": " << 1000*((double)(_clock_end_-_clock_begin_)/CLOCKS_PER_SEC) << "ms" << endl;
 
-
+#define PII pair<int, int>
+#define PLL pair<long long, long long>
 FUNCTOR(pii_first, bool, (PII p1, PII p2), {return p1.first < p2.first;})
 FUNCTOR(pii_second, bool, (PII p1, PII p2), {return p1.second < p2.second;})
+
+// ##################################################################
+// ########################### geometry #############################
+// ##################################################################
+
+// ManHattanDis returns the Manhattan distance of these two points.
+int ManHattanDis(PII p1, PII p2) {
+    return abs(p1.first-p2.first) + abs(p1.second-p2.second);
+}
 
 // ##################################################################
 // ######################## data structures #########################
