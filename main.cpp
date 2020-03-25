eb72adfb9b8816572a335e6e173ae35e0260d44d
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-03-25 10:58:24 +0800

    update segment tree

--- main.cpp
+++ main.cpp
@@ -49,103 +49,66 @@ int ManHattanDis(PII p1, PII p2) {
 // ######################## data structures #########################
 // ##################################################################
 
-#define LS(k) (k*2)
-#define RS(k) (k*2+1)
-class SegmentTree {
-public:
-    LL *sum;
-    int *L, *R, *rights, *flags;
-    SegmentTree(int n) {
-        sum = new LL[n*4];
-        L = new int[n*4];
-        R = new int[n*4];
-        rights = new int[n*4];
-        flags = new int[n*4];
-    }
-    
-    ~SegmentTree() {
-    }
+// segment tree
+VEC<int> stl_, str_;
+void stBuild(int n) {
+    stl_.resize(n*4);
+    str_.resize(n*4);
+}
 
-    void Build(int k, int l, int r, LL v) {
-        L[k] = l;
-        R[k] = r;
-        flags[k] = -1;
-        if (l == r) {
-            sum[k] = v;
-            rights[k] = v;
-            return;
-        }
-        int mid = (l+r) >> 1;
-        Build(LS(k), l, mid, v);
-        Build(RS(k), mid + 1, r, v);
-        sum[k] = sum[LS(k)] + sum[RS(k)];
-        rights[k] = rights[RS(k)];
+void stBuild_(int nd, int l, int r) {
+    stl_[nd] = l;
+    str_[nd] = r;
+    if (l == r) {
+        // TODO
+        return;
     }
+    int mid = (l + r) >> 1;
+    stBuild_(nd*2, l, mid);
+    stBuild_(nd*2+1, mid+1, r);
+    // TODO
+}
 
-    void cal_and_push(int k) {
-        if (flags[k] == -1) {
-            return;
-        }
-        sum[k] = (LL)(flags[k]) * (LL)(R[k]-L[k]+1);
-        rights[k] = flags[k];
-        if (R[k] > L[k]) {
-            flags[LS(k)] = flags[k];
-            flags[RS(k)] = flags[k];
-        }
-        flags[k] = -1;
+void stRangeQuery(int nd, int l, int r) {
+    if (stl_[nd] == l && str_[nd] == r) {
+        // TODO
+        return;
     }
+    int mid = (stl_[nd] + str_[nd]) >> 1;
+    if (r <= mid) stRangeQuery(nd*2, l, r);
+    else if (l > mid) stRangeQuery(nd*2+1, l, r);
+    else {
+        stRangeQuery(nd*2, l, mid);
+        stRangeQuery(nd*2+1, mid+1, r);
+        // TODO
+    }
+}
 
-    LL Sum(int k, int l, int r) {
-        cal_and_push(k);
-        if (L[k] == l && R[k] == r) {
-            return sum[k];
-        }
-        int mid = (L[k]+R[k]) >> 1;
-        if (r <= mid) return Sum(LS(k), l, r);
-        else if (l > mid) return Sum(RS(k), l, r);
-        return Sum(LS(k), l, mid) + Sum(RS(k), mid+1, r);
+void stPointUpdate(int nd, int pos) {
+    if (stl_[nd] == str_[nd]) {
+        // TODO
+        return;
     }
+    int mid = (stl_[nd] + str_[nd]) >> 1;
+    if (pos <= mid) stPointUpdate(nd*2, pos);
+    else stPointUpdate(nd*2+1, pos);
+    // TODO
+}
 
-    void Set(int k, int l, int r, int v) {
-        if (L[k] == l && R[k] == r) {
-            flags[k] = v;
-            cal_and_push(k);
-            return;
-        }
-        int mid = (L[k]+R[k]) >> 1;
-        if (r <= mid) Set(LS(k), l, r, v);
-        else if (l > mid) Set(RS(k), l, r, v);
-        else Set(LS(k), l, mid, v), Set(RS(k), mid+1, r, v);
-        sum[k] = sum[LS(k)] + sum[RS(k)];
-        rights[k] = rights[RS(k)];
+void stRangeUpdate(int nd, int l, int r) {
+    if (stl_[nd] == l && str_[nd] == r) {
+        // TODO
+        return;
     }
-
-    int LowerBound(int k, int l, int r, int v) {
-        cal_and_push(k);
-        if (l == r) {
-            if (sum[k] < v) return r+1;
-            return l;
-        }
-        int mid = (L[k]+R[k]) >> 1;
-        if (r <= mid) return LowerBound(LS(k), l, r, v);
-        else if (l > mid) return LowerBound(RS(k), l, r, v);
-        else {
-            cal_and_push(LS(k));
-            if (rights[LS(k)] < v) {
-                return LowerBound(RS(k), mid + 1, r, v);
-            }
-            return LowerBound(LS(k), l, mid, v);
-        }
-        return r+1;
+    int mid = (stl_[nd] + str_[nd]) >> 1;
+    if (r <= mid) stRangeUpdate(nd*2, l, r);
+    else if (l > mid) stRangeUpdate(nd*2+1, l, r);
+    else {
+        stRangeUpdate(nd*2, l, mid);
+        stRangeUpdate(nd*2+1, mid+1, r);
     }
-
-    void WalkPrint(int k) {
-        printf("%d %d %d %d\n", k, L[k], R[k], rights[k]);
-        if (L[k] == R[k]) return;
-        WalkPrint(LS(k));
-        WalkPrint(RS(k));
-    }
-};
+    // TODO
+}
 
 // ##################################################################
 // ############################# others #############################
