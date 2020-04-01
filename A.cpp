6226070cc3f69d2614f82e4548a53215d04d880b
Yuanjia Zhang <zhangyuanjia@pingcap.com>
2020-04-01 10:36:00 +0800

    fixup

---
+++ A.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+#include <stdlib.h>
+#include <map>
+#include <set>
+#include <cstring>
+#include <queue>
+#include <stack>
+#include <assert.h>
+#include <deque>
+#include <list>
+#include <math.h>
+using namespace std;
+
+#define MAXN 500005
+#define LL long long
+#define REP(i, n) for(int i = 0; i < n; i ++)
+#define FOR(i, b, e) for(int i = b; i <= e; i++)
+#define FORIT(begin, end) for(auto it = begin; it != end; it++)
+#define FOREACH(x, xs) for(auto x : xs)
+#define FORCASE() int _t_; cin >> _t_; for(int _i_ = 0; _i_ < _t_; _i_++)
+#define COPY(dst, src, n) for(int i = 0; i < n; i++) {*dst=*src;dst++;src++;}
+#define RESET(begin, end, v) for(auto it = begin; it != end; it++) *it=v;
+#define VEC vector
+#define INPUT_VEC(vs, n) vs.resize(n); for(int i = 0; i < n; i++) cin >> vs[i];
+#define OUTPUT_VEC(vs) for(auto it=vs.begin(); it != vs.end(); it++) cout << *it << " "; cout << endl;
+#define OUTPUT_BE(begin, end) for(auto it = begin; it != end; it++) cout << *it << " "; cout << endl;
+#define FUNCTOR(name, ret, args, body) struct name {ret operator() args body};
+#define CLOCK_BEGIN() clock_t _clock_begin_ = clock();
+#define CLOCK_END() clock_t _clock_end_ = clock(); cout << "clock" << ": " << 1000*((double)(_clock_end_-_clock_begin_)/CLOCKS_PER_SEC) << "ms" << endl;
+
+#define PII pair<int, int>
+#define PLL pair<long long, long long>
+FUNCTOR(pii_first, bool, (PII p1, PII p2), {return p1.first < p2.first;})
+FUNCTOR(pii_second, bool, (PII p1, PII p2), {return p1.second < p2.second;})
+
+int main() {
+    FORCASE() {
+    }
+}
---
+++ solutions/1322B.cpp
@@ -0,0 +1,107 @@
+/*
+
+题目：https://codeforces.ml/problemset/problem/1322/B
+
+解法 1：
+拆位处理，先求最后一位是不是 1，再求倒数第二位是不是 1，以此类推。
+计算低 k 位是不是 1 的方法，就是对于每个数 a，求和他相加后这一位为 1 的 count，然后把所有 count 相加。
+如果 sum_count&1 == 1，则说明这一位是 1。
+
+对于每次处理 a 的 k 位，如果
+1. a[k]==1：则和他相加后 k 为 1 的数 b 有两种情况：
+    1）b[k]==0 且 b[0:k-1]+a[0:k-1] 不会产生进位
+    2）b[k]==1 且 b[0:k-1]+a[0:k-1] 会有进位
+2. a[k]==0：则同样 b 有两种情况：
+    1）b[k]==0 且 b[0:k-1]+a[0:k-1] 会产生进位
+    2）b[k]==1 且 b[0:k-1]+a[0:k-1] 不会有进位
+为了快速处理满足 a[k] 条件的 b 有多少个，把所有的 b 安位拆分后维护在一棵二叉树上。
+二叉树的边表示某一位是 0 还是 1，节点维护一个 sum 表示进过改点的个数。
+
+最后的复杂度为 O(logA*logA*n): 不过很可惜最后 TLE 了
+
+int n;
+VEC<int> as;
+
+struct node {
+    VEC<int> son;
+    int sum;
+};
+
+int cur = 0;
+node tree[400009*26];
+
+int new_node() {
+    tree[cur].son.resize(2);
+    tree[cur].son[0] = -1;
+    tree[cur].son[1] = -1;
+    tree[cur].sum = 0;
+    return cur++;
+}
+
+void rm_tree() {
+    cur = 0;
+}
+
+void insert_tree(int nd, int v, int k) {
+    tree[nd].sum++;
+    if (k == -1) return;
+    int b = (v >> k) & 1;
+    if (b == 0) {
+        if (tree[nd].son[0] == -1) tree[nd].son[0] = new_node();
+        insert_tree(tree[nd].son[0], v, k-1);
+    } else {
+        if (tree[nd].son[1] == -1) tree[nd].son[1] = new_node();
+        insert_tree(tree[nd].son[1], v, k-1);
+    }
+    return;
+}
+
+int find_push(int nd, int v, int k) {
+    if (nd == -1 || k == -1) return 0;
+    int b = (v >> k) & 1;
+    int ret = 0;
+    if (b == 1 && tree[nd].son[1] != -1) {
+        ret += tree[tree[nd].son[1]].sum;
+    }
+    return ret + find_push(tree[nd].son[!b], v, k-1);
+}
+
+int find_nonpush(int nd, int v, int k) {
+    if (nd == -1) return 0;
+    if (k == -1) return tree[nd].sum;
+    int b = (v >> k) & 1;
+    int ret = 0;
+    if (b == 0 && tree[nd].son[0] != -1) {
+        ret += tree[tree[nd].son[0]].sum;
+    }
+    return ret + find_nonpush(tree[nd].son[!b], v, k-1);
+}
+
+int main() {
+    cin >> n;
+    INPUT_VEC(as, n)
+
+    int result = 0;
+    REP(k, 26) {
+        int cnt = 0;
+        int root = new_node();
+        FOREACH(a, as) {
+            int b = (a >> k) & 1;
+            cnt += find_push(tree[root].son[b], a, k-1);
+            cnt += find_nonpush(tree[root].son[!b], a, k-1);
+            insert_tree(root, a, k);
+        }
+        if (cnt & 1) result += (1 << k);
+        rm_tree();
+    }
+    cout << result << endl;
+    return 0;
+}
+
+*/
+
+/*
+
+解法 2：
+
+*/
